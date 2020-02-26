c748ba2751eaae2d8c23af7403b66225ab860290
vrtpoint <vrtpoint@yahoo.com>
2020-02-26 22:14:41 +0300

    added print type of the fixture

--- test_suites/string/conftest.py
+++ test_suites/string/conftest.py
@@ -3,16 +3,14 @@ import pytest
 
 @pytest.fixture(scope="function")
 def сreate_phrase(request):
-    #print(f"\n from {request.scope} fixture!")
+    print(f"\n from {request.scope} fixture!")
     phrase = "Pytest - тестовый фреймворк"
-    print(phrase)
     return phrase
 
 @pytest.fixture(scope="module")
 def сoncatenate_phrase(request):
-    #print(f"\n from {request.scope} fixture!")
+    print(f"\n from {request.scope} fixture!")
     part_1 = "Pytest -"
     part_2 = "тестовый фреймворк"
     phrase = part_1 + " " + part_2
-    print(phrase)
     return phrase
