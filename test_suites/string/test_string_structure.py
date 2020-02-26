63e3bd1acc97eef7bb40532cf1a53fda21dcd5c0
vrtpoint <vrtpoint@yahoo.com>
2020-02-26 22:13:08 +0300

    added a tests

--- test_suites/string/test_string_structure.py
+++ test_suites/string/test_string_structure.py
@@ -11,4 +11,15 @@ def test_concatenation_phrase(сoncatenate_phrase):
     print(сoncatenate_phrase)
     assert сoncatenate_phrase == "Pytest - тестовый фреймворк"
 
+#Тест сравнения типов данных фраз
+@pytest.mark.parametrize('phrase', ["Pytest - тестовый фреймворк"])
+def test_comparison_types(сreate_phrase, phrase):
+    assert type(сreate_phrase) == type(phrase)
 
+#Тест корректности среза фразы
+def test_trimming_phrase(сreate_phrase):
+    assert сreate_phrase[:6] == "Pytest"
+
+#Тест положения регистра фразы
+def test_upper_case(сreate_phrase):
+    assert сreate_phrase.upper() == "PYTEST - ТЕСТОВЫЙ ФРЕЙМВОРК"
