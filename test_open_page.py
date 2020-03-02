8eb06d755bff04554c5b0da36d55f8adc8b27a50
workstation <vrtpoint@yahoo.com>
2020-03-02 16:55:46 +0300

    deleted the test

--- test_open_page.py
+++
@@ -1,6 +0,0 @@
-# Тест открытия главной страницы opencart
-def test_open_page(driver, url_param):
-    driver.get(url_param)
-    assert driver.title == 'Your Store'
-    print('тест пройден')
-    driver.close()
