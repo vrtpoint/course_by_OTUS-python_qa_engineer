912a74c3868a4e6d8beab61fc3d4c2c10853845d
vrtpoint <vrtpoint@yahoo.com>
2020-04-05 22:55:45 +0300

    edited conditions for starting browser

--- conftest.py
+++ conftest.py
@@ -6,41 +6,26 @@ import pytest
 def pytest_addoption(parser):
     parser.addoption("--opencart_url", action="store", default="http://localhost/", help="This is opencart_url", required=False)
     parser.addoption("--browser", action="store", default="firefox", help="This is request browser", required=False)
-    parser.addoption("--headless", action="store", default=False, help="This is headless mode")
+    parser.addoption("--implicitly_wait", action="store", default="3", help="waiting time in the seconds", required=False)
 
-# Фикстура запуска различных браузеров в обычном и headless режимах
+# Фикстура запуска различных браузеров
 @pytest.fixture()
 def driver(request):
     browser = request.config.getoption("--browser")
-    headless = request.config.getoption("--headless")
-    if browser == 'chrome':
-        if headless == False:
-            wd = webdriver.Chrome()
-            yield wd
-            wd.quit()
-        else:
-            options = webdriver.ChromeOptions()
-            options.add_argument("--headless")
-            wd = webdriver.Chrome(options=options)
-            yield wd
-            wd.quit()
-    elif browser == 'firefox':
-        if headless == False:
-            wd = webdriver.Firefox()
-            yield wd
-            wd.quit()
-        else:
-            options = webdriver.FirefoxOptions()
-            options.add_argument("--headless")
-            wd = webdriver.Firefox(options=options)
-            yield wd
-            wd.quit()
-    elif browser == 'opera':
-        wd = webdriver.Opera()
-        yield wd
-        wd.quit()
+    wait = request.config.getoption("--implicitly_wait")
+    if browser == "chrome":
+        driver = webdriver.Chrome()
+    elif browser == "firefox":
+        driver = webdriver.Firefox()
+    elif browser == "opera":
+        driver = webdriver.Opera()
+    elif browser == "safari":
+        driver = webdriver.Safari()
+    else:
+        raise Exception(f"{request.param} is not supported!")
 
-# Фикстура возвращает аргумент выбора url командной строки
-@pytest.fixture()
-def url_param(request):
-    return request.config.getoption("--opencart_url")
+    driver.implicitly_wait(wait)
+    request.addfinalizer(driver.close)
+    driver.get(request.config.getoption("--opencart_url"))
+
+    return driver
