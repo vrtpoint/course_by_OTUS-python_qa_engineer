9c7e986d0d62e92aeba084e3d88139be1766a521
vrtpoint <vrtpoint@yahoo.com>
2020-04-13 00:32:32 +0300

    edited and made methods for page: add_product_item, edit_product_item, delete_product_item

--- resources/pages/admin_panel/product_page.py
+++ resources/pages/admin_panel/product_page.py
@@ -1,4 +1,5 @@
 """Модуль с методами для работы с продуктами в панеле администратора"""
+from selenium.webdriver.common.alert import Alert
 from resources.locators.admin_panel.home import SideBar, Products
 from resources.common.base_actions import BaseActions
 
@@ -10,18 +11,33 @@ class ProductPage(BaseActions):
 
         def add_product_item(self):
             self._click(*self.sidebar.catalog_list)
-            assert self._driver.find_element(*self.sidebar.catalog_list).is_displayed()
             self._click(*self.sidebar.products_link)
-            assert self._driver.find_element(*self.sidebar.products_link).is_displayed()
             self._click(*self.product.add_product_item)
-            self._input(*self.product.product_name_field, value='тест')
-            self._input(*self.product.description_field, value='тест')
-            self._input(*self.product.meta_tag_title, value='тест')
+            self._input(*self.product.product_name_field, value='test')
+            self._input(*self.product.description_field, value='test')
+            self._input(*self.product.meta_tag_title, value='test')
             self._click(*self.product.data_link)
-            self._input(*self.product.model_field, value='тест')
+            self._input(*self.product.model_field, value='test')
             self._click(*self.product.submition_button)
+            assert self._driver.find_element(*self.product.operation_status).text.split("\n")[0] == 'Success: You have modified products!'
 
         def edit_product_item(self):
             self._click(*self.sidebar.catalog_list)
             self._click(*self.sidebar.products_link)
-            self._click(*self.product.add_product_item)[1]
+            concrete_product = self._driver.find_elements(*self.product.product_item)[19]
+            concrete_product.click()
+            edit_element = self._driver.find_elements(*self.product.edit_product_item)[18]
+            edit_element.click()
+            self._input(*self.product.product_name_field, value='test_product')
+            self._input(*self.product.description_field, value='test_product')
+            self._click(*self.product.submition_button)
+            assert self._driver.find_element(*self.product.operation_status).text.split("\n")[0] == 'Success: You have modified products!'
+
+        def delete_product_item(self):
+            self._click(*self.sidebar.catalog_list)
+            self._click(*self.sidebar.products_link)
+            concrete_product = self._driver.find_elements(*self.product.product_item)[19]
+            concrete_product.click()
+            self._click(*self.product.delete_product_item)
+            Alert(self._driver).accept()
+            assert self._driver.find_element(*self.product.operation_status).text.split("\n")[0] == 'Success: You have modified products!'
--- tests/admin_panel/test_products.py
+++ tests/admin_panel/test_products.py
@@ -1,11 +1,11 @@
+"""Модуль с тестами товарной позиции"""
 from resources.common.base_set_up import BaseSetUp
 from resources.pages.admin_panel.auth import AdminPanelAuthorizationPage
 from resources.pages.admin_panel.product_page import ProductPage
 from decouple import config
 
 
-class TestShoppingCart(BaseSetUp):
-
+class TestProducts(BaseSetUp):
 
     def test_addition_product_item(self):
         self.driver.get(config('url') + '/admin')
@@ -18,5 +18,12 @@ class TestShoppingCart(BaseSetUp):
         self.driver.get(config('url') + '/admin')
         auth_page = self.get_page(AdminPanelAuthorizationPage)
         auth_page.login(config('admin_username'), config('admin_password'))
-        sidebar = self.get_page(ProductPage)
-        sidebar.edit_product_item()
+        product = self.get_page(ProductPage)
+        product.edit_product_item()
+
+    def test_deletion_product_item(self):
+        self.driver.get(config('url') + '/admin')
+        auth_page = self.get_page(AdminPanelAuthorizationPage)
+        auth_page.login(config('admin_username'), config('admin_password'))
+        product = self.get_page(ProductPage)
+        product.delete_product_item()
