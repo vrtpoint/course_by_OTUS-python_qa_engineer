1259ecfaec07441a636aa054966e2d4f0c7dbbd3
vrtpoint <vrtpoint@yahoo.com>
2020-06-16 14:10:47 +0300

    added paramaters and method to work with remote browser

--- conftest.py
+++ conftest.py
@@ -5,29 +5,23 @@ import pytest
 
 # Добавление различных аргументов командной строки
 def pytest_addoption(parser):
-    parser.addoption("--browser", action="store", default="firefox", help="This is request browser", required=False)
+    parser.addoption("--browser", action="store", default="chrome",
+                     choices=["chrome", "firefox", "safari", "opera"])
+    parser.addoption("--executor", action="store", default="192.168.1.44")
     parser.addoption("--implicitly_wait", action="store", default="3", help="waiting time in the seconds", required=False)
 
-# Фикстура запуска различных браузеров
-@pytest.fixture()
+
+# Фикстура запуска удаленного запуска браузера
+@pytest.fixture
 def driver(request):
     browser = request.config.getoption("--browser")
     wait = request.config.getoption("--implicitly_wait")
-    if browser == "chrome":
-        driver = webdriver.Chrome()
-    elif browser == "firefox":
-        driver = webdriver.Firefox()
-    elif browser == "opera":
-        driver = webdriver.Opera()
-    elif browser == "safari":
-        driver = webdriver.Safari()
-    else:
-        raise Exception(f"{request.param} is not supported!")
+    executor = request.config.getoption("--executor")
+    driver = webdriver.Remote(command_executor=f"http://{executor}:4444/wd/hub",
+                          desired_capabilities={"browserName": browser})
 
     driver.maximize_window()
     driver.implicitly_wait(wait)
     request.addfinalizer(driver.close)
 
     return driver
-
-
