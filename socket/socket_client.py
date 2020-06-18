05729784f04accf711832416dc87dbc8e5eff18f
vrtpoint <vrtpoint@yahoo.com>
2020-06-18 14:48:58 +0300

    added realisation for socket and test

---
+++ socket/socket_client.py
@@ -0,0 +1,55 @@
+import socket
+import ssl
+
+
+def ssl_create():
+    # Создаем SSL-подключение:
+    context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+    context.verify_mode = ssl.CERT_REQUIRED
+    context.check_hostname = True
+    context.load_default_certs()
+    return context
+
+def socket_create(context, host):
+    # Создаем Socket-клиент:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    sock = context.wrap_socket(sock, server_hostname=host)
+    return sock
+
+def socket_connect(sock, host, port):
+    # Подключаемся к серверу:
+    sock.connect((host, port))
+
+def request_create(method, host, headers):
+    # Создаем запрос:
+    request = f'{method} / HTTP/1.1\n\n' + f'Host: {host}\n\n' + f'{headers}'
+    return request
+
+def socket_send(sock, request):
+    # Отправляем запрос:
+    sock.send(request.encode())
+    return sock
+
+def socket_receive(sock):
+    # Конец ответа, до которого будем искать:
+    end_response = b'</html>'
+    # Итоговый ответ:
+    total_response = []
+    while True:
+        response = sock.recv(8192)
+        if end_response in response:
+            total_response.append(response[:response.find(end_response)])
+            break
+        total_response.append(response)
+        if len(total_response) > 1:
+            # Проверяем, если end_response был разбит
+            last_pair = total_response[-2] + total_response[-1]
+            if end_response in last_pair:
+                total_response[-2] = last_pair[:last_pair.find(end_response)]
+                total_response.pop()
+                break
+    return ''.join(str(x) for x in total_response)
+
+def close_connect(sock):
+    # Закрываем соединение:
+    sock.close()
---
+++ socket/test_socket_client.py
@@ -0,0 +1,36 @@
+from socket_client import ssl_create, socket_create, socket_connect, \
+    request_create, socket_send, socket_receive, close_connect
+import yaml
+
+
+conf = yaml.safe_load(open('configuration.yml'))
+
+method = conf['request']['method']
+port = conf['request']['port']
+host = conf['request']['host']
+headers = conf['request']['headers']
+
+
+def test_get_response():
+    # Создаем SSL-подключение:
+    context = ssl_create()
+    # Cоздаем Socket-client:
+    sock = socket_create(context, host)
+    # Подключаемся к серверу:
+    socket_connect(sock, host, port)
+    # Создаем запрос:
+    request = request_create(method, host, headers)
+    # Отправляем запрос:
+    sock = socket_send(sock, request)
+    # Получаем ответ:
+    response_data = socket_receive(sock)
+    # Закрываем соединение:
+    close_connect(sock)
+    # Проверяем, что мы получили успешный ответ:
+    if 'HTTP/1.1' and '200' and 'OK' in response_data:
+        print(f'{method}: The resource has been fetched and is transmitted'
+              f' in the message body\n', response_data)
+    elif ('HTTP/1.1' and '200' and 'OK') not in response_data:
+        assert False, '400 Bad Request '
+    else:
+        assert False, 'Something wrong'
